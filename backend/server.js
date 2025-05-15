fbb8181dc99662043696878a217852d6b980ffb9
Emmanuel <e.fkumah@gmail.com>
2025-05-15 19:21:50 +0000

    Add environment variables and morgan to log output during development

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,22 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from "express";
+import morgan from "morgan";
 const app = express();
+app.use(express.json());
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
-app.get('/', (req, res)=>{
-res.send('Hello world')
-})
+app.post("/", (req, res) => {
+  console.log(req);
+  res.json({ message: "Data received", data: req.body });
+});
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
-app.listen(5100, ()=>{
-  console.log('server is running on port 5100...')  
-})
+const port = process.env.PORT || 5100;
+app.listen(port, () => {
+  console.log(`server is running on port ${port}...`);
+});
