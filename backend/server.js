42e1313ef63340b832d197dada78bfe453471d04
Emmanuel <e.fkumah@gmail.com>
2025-05-15 20:19:53 +0000

    feat: create CS member

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,71 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import morgan from "morgan";
+import { nanoid } from "nanoid";
+
 const app = express();
 app.use(express.json());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+//get all cs members
+const csMembers = [
+  {
+    id: nanoid(),
+    firstName: "Corty",
+    lastName: "Dixson",
+    dateOfBirth: "9/28/2021",
+    gender: "Male",
+    fathersName: "Corty Dixson",
+    mothersName: "Corty Dixson",
+    residence: "Room 1887",
+    contact: "602-623-5247",
+    image: "http://dummyimage.com/186x100.png/ff4444/ffffff",
+    age: 10,
+  },
+  {
+    id: nanoid(),
+    firstName: "Valencia",
+    lastName: "Ride",
+    dateOfBirth: "12/23/2019",
+    gender: "Female",
+    fathersName: "Valencia Ride",
+    mothersName: "Valencia Ride",
+    residence: "Apt 234",
+    contact: "529-649-3079",
+    image: "http://dummyimage.com/240x100.png/ff4444/ffffff",
+    age: 4,
+  },
+];
+
+app.get("/api/v1/cs-members", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: csMembers.length,
+    data: {
+      csMembers,
+    },
+  });
+});
+//create a new cs member
+app.post("/api/v1/cs-member", (req, res) => {
+  const { firstName, lastName, dateOfBirth } = req.body;
+  if (!firstName || !lastName || !dateOfBirth) {
+    return res.status(400).json({
+      status: "fail",
+      message: "Please provide firstName, lastName and dateOfBirth",
+    });
+  }
+  const newMember = { id: nanoid(), ...req.body };
+  csMembers.push(newMember);
+  res.status(201).json({
+    status: "success",
+    data: {
+      csMember: newMember,
+    },
+  });
+});
 
 app.post("/", (req, res) => {
   console.log(req);
