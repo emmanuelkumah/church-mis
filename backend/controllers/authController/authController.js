8e3add879a0d414f49bca77de1e6446d8168f66c
Emmanuel <e.fkumah@gmail.com>
2025-05-28 14:14:16 +0000

    Feat: persist created user in the database

--- backend/controllers/authController/authController.js
+++ backend/controllers/authController/authController.js
@@ -1,5 +1,13 @@
+import UserModel from "../../models/UserModel.js";
+import { StatusCodes } from "http-status-codes";
 export const register = async (req, res) => {
-  res.send("Register endpoint hit");
+  const user = await UserModel.create(req.body);
+  res.status(StatusCodes.CREATED).json({
+    status: "success",
+    data: {
+      user,
+    },
+  });
 };
 
 export const login = async (req, res) => {
