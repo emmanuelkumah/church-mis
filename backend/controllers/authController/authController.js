540226a3b967204e980b6ad03ee0b228107aa66f
Emmanuel <e.fkumah@gmail.com>
2025-05-28 15:00:06 +0000

    Add hash password when creating an account

--- backend/controllers/authController/authController.js
+++ backend/controllers/authController/authController.js
@@ -1,10 +1,15 @@
 import UserModel from "../../models/UserModel.js";
 import { StatusCodes } from "http-status-codes";
-
+import bcrypt from "bcryptjs";
 // Register and login controllers for user authentication
 export const register = async (req, res) => {
   const isFirstAccount = (await UserModel.countDocuments({})) === 0;
   req.body.role = isFirstAccount ? "admin" : "user";
+  //random value added to the password to make it more secure
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  req.body.password = hashedPassword;
+  // Create a new user with the provided data
   const user = await UserModel.create(req.body);
   res.status(StatusCodes.CREATED).json({
     status: "success",
