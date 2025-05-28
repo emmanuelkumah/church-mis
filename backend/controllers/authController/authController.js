eaf90765559478175f4716cc1e18690d1911d823
Emmanuel <e.fkumah@gmail.com>
2025-05-28 14:39:25 +0000

    Feat: add admin role to user creation

--- backend/controllers/authController/authController.js
+++ backend/controllers/authController/authController.js
@@ -1,6 +1,10 @@
 import UserModel from "../../models/UserModel.js";
 import { StatusCodes } from "http-status-codes";
+
+// Register and login controllers for user authentication
 export const register = async (req, res) => {
+  const isFirstAccount = (await UserModel.countDocuments({})) === 0;
+  req.body.role = isFirstAccount ? "admin" : "user";
   const user = await UserModel.create(req.body);
   res.status(StatusCodes.CREATED).json({
     status: "success",
