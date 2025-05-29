4f006a84fff952a191f9b23e4d0ce33c3caa06e2
Emmanuel <e.fkumah@gmail.com>
2025-05-29 00:05:26 +0000

    Add jwt secret to env file

--- backend/utils/tokenUtils.js
+++ backend/utils/tokenUtils.js
@@ -1,8 +1,8 @@
 import jwt from "jsonwebtoken";
 
 export const createToken = (payload) => {
-  const token = jwt.sign(payload, "secretKey", {
-    expiresIn: "1d", // Token will expire in 1 hour
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN || "30d", // Default to 30 days if not set
   });
 
   return token;
