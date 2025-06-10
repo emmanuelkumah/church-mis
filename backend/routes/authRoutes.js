8e3e3f1ee04cc54710ce38aaf59f4e66634eba97
Emmanuel <e.fkumah@gmail.com>
2025-06-10 12:33:19 +0000

    Add logout out controller

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   register,
   login,
+  logout,
 } from "../controllers/authController/authController.js";
 import { validateUserRegistration } from "../middleware/validationMiddleware.js";
 import { validateUserLogin } from "../middleware/validationMiddleware.js";
@@ -10,6 +11,7 @@ const router = Router();
 
 router.post("/register", validateUserRegistration, register);
 router.post("/login", validateUserLogin, login);
+router.get("/logout", logout);
 // Export the router to be used in the main server file
 export default router;
 // This file defines the authentication routes for user registration and login.
