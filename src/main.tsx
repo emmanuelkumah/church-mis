0a4202e9e7dd15b3c25592db33fc9f6bb29be694
Emmanuel <e.fkumah@gmail.com>
2025-06-19 21:30:18 +0000

    Add proxy to enable frontend make api request to the server

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 // import App from './App.tsx'
 import RouteContainer from "./routes/RouteContainer.tsx";
-
+fetch("/api/v1/test")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+// Uncomment the line below to see the data from the test route in the console
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouteContainer />
